7a5efd7ced7561993b8c1442c0b6ee194c71a3c4
Ahmed Raza <bhaia9036@gmail.com>
2024-10-31 14:19:00 -0200

    Initial Commit

---
+++ src/utils/data.ts
@@ -0,0 +1,28 @@
+const services = [
+    {
+        title: "Load Matching",
+        description: "We find quality loads and match them with available drivers, maximizing each mile.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Route Coordination",
+        description: "Every route detail is handled—timing, stops, updates—so it all flows smoothly.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Hands-Free Communication",
+        description: "From pick-up to delivery, ClearRoad coordinates, letting drivers focus on driving.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "On-the-Road Support",
+        description: "If an issue arises, ClearRoad steps in to find solutions fast.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+];
+
+export { services };
